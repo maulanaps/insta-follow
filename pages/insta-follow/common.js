98c3d8f53c988f4d1459ebc4ac8d9cf6b0ebc38d
Maulana Putra <maulana.putra@sg-edts.com>
2023-09-17 16:24:43 +0700

    dummy file

---
+++ pages/insta-follow/common.js
@@ -0,0 +1,346 @@
+export const dummy = [
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841400056573270",
+        "text_post_app_joiner_number": 11322368,
+        "pk": 1489281885,
+        "pk_id": "1489281885",
+        "username": "muzammilhb",
+        "full_name": "Muzammil Hasballah 🇮🇩",
+        "is_private": false,
+        "is_verified": true,
+        "profile_pic_id": "2702394997004365581_1489281885",
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/253826690_1057240281768748_6367411398720407660_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=1&_nc_ohc=WKTciHMlVBUAX8Bc0xn&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfCafo1IMqoWPPF1JLyvpQLDSVCFxpdbmyKWhDOmk5liUQ&oe=64C7D197&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 0,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 0,
+        "is_favorite": false
+    },
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841400624184050",
+        "text_post_app_joiner_number": 404,
+        "pk": 259220806,
+        "pk_id": "259220806",
+        "username": "9gag",
+        "full_name": "9GAG: Go Fun The World",
+        "is_private": false,
+        "is_verified": true,
+        "profile_pic_id": "3023530294053359432_259220806",
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/326894746_1343008276240985_2214320890528074261_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=1&_nc_ohc=_62i2V3225IAX_hVXsr&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfBezOhsMnPE5w4nY7ihnGjR6cTlaRTNeRfYRVFturJ_Cg&oe=64C7B8CA&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 0,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 0,
+        "is_favorite": false
+    },
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841402244878607",
+        "pk": 2228858829,
+        "pk_id": "2228858829",
+        "username": "hanan_attaki",
+        "full_name": "Founder: Shift Pemuda Hijrah movement",
+        "is_private": false,
+        "is_verified": true,
+        "profile_pic_id": "2657527985687983885_2228858829",
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/241373057_4134749553313712_3009136101378508167_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=1&_nc_ohc=5OdewDcDMeAAX95sRad&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfDQ7O9EkpVe0O__uPhm496byGnb5vVppJbl3TorJ-cFcw&oe=64C647E9&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 0,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 0,
+        "is_favorite": false
+    },
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841400575216841",
+        "pk": 1717843173,
+        "pk_id": "1717843173",
+        "username": "widyutami2",
+        "full_name": "Widy Utami",
+        "is_private": true,
+        "is_verified": false,
+        "profile_pic_id": "2773979000676873279_1717843173",
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/273981880_268352968749192_4730270807456552226_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=100&_nc_ohc=LpKDNBuo6ZUAX_Tem1o&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfCAMuDbMPcE3KruQZ1f8irItmaoxsk6aBAZQdgucGiHPw&oe=64C6CC17&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 0,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 0,
+        "is_favorite": false
+    },
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841401739899612",
+        "text_post_app_joiner_number": 14852499,
+        "pk": 1255225996,
+        "pk_id": "1255225996",
+        "username": "masmono08",
+        "full_name": "MASMONO",
+        "is_private": false,
+        "is_verified": true,
+        "profile_pic_id": "3007331319466997857_1255225996",
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/323751250_613573400770284_3229731485709114552_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=100&_nc_ohc=X_5WqYM8EgsAX-dg9eS&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfATkF9fYcfpfClO_1Y1UAOUBmciSyc10l-Yj3JGWt4l9w&oe=64C7EE02&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 0,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 0,
+        "is_favorite": false
+    },
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841400416093641",
+        "pk": 1513037869,
+        "pk_id": "1513037869",
+        "username": "teladan.rasul",
+        "full_name": "teladan rasul",
+        "is_private": false,
+        "is_verified": false,
+        "profile_pic_id": "2282146027456483145_1513037869",
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/92510791_752592981811304_653821613044662272_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=1&_nc_ohc=jeTWjwGewzAAX8xPkGk&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfAb5AOxMkX9K7CropHHNupZ79547Qd6XUzTjlLLKPaJQg&oe=64C6D0B0&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 0,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 0,
+        "is_favorite": false
+    },
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841400255874250",
+        "text_post_app_joiner_number": 4945575,
+        "pk": 272008529,
+        "pk_id": "272008529",
+        "username": "felixsiauw",
+        "full_name": "Felix Siauw",
+        "is_private": false,
+        "is_verified": true,
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/10536894_450673215093913_264335098_a.jpg?_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=1&_nc_ohc=p82CYMAiBDsAX-wUlkC&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfCIYcCdlv4gBEArJH1nr2SR2AgveNbbe4fPjbCeYKh02g&oe=64C7AA3A&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 0,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 0,
+        "is_favorite": false
+    },
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841400498720633",
+        "text_post_app_joiner_number": 15292157,
+        "pk": 40528624,
+        "pk_id": "40528624",
+        "username": "ridwankamil",
+        "full_name": "Ridwan Kamil",
+        "is_private": false,
+        "is_verified": true,
+        "profile_pic_id": "3142624325967465148_40528624",
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/358359143_1410371089744072_1325241223478194766_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=1&_nc_ohc=Ekn-eDlbPUAAX-uNkw-&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfBbN1gtSJaKftV2onOIB7fTVX5nhmRT1fLiFVOuBl_zvQ&oe=64C7A515&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 0,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 0,
+        "is_favorite": false
+    },
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841400573960012",
+        "text_post_app_joiner_number": 672534,
+        "pk": 787132,
+        "pk_id": "787132",
+        "username": "natgeo",
+        "full_name": "National Geographic",
+        "is_private": false,
+        "is_verified": true,
+        "profile_pic_id": "2831563011753844382_787132",
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/279785786_129563936340251_3274963801959382830_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=1&_nc_ohc=VWNdkdbcO3EAX_HvB94&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfAjh94JqOJCYTR9SJcqE28a2jlEE1F1z3g8K5XM93c2aA&oe=64C7050B&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 2,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 1690387924,
+        "is_favorite": false
+    },
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841401567088594",
+        "text_post_app_joiner_number": 3363093,
+        "pk": 550068236,
+        "pk_id": "550068236",
+        "username": "aniesbaswedan",
+        "full_name": "Anies Rasyid Baswedan",
+        "is_private": false,
+        "is_verified": true,
+        "profile_pic_id": "2957982321842067891_550068236",
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/312833161_126668086666560_2843690090521022518_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=1&_nc_ohc=sk6tA37u3jsAX_NgbkW&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfB3PaYci7klaYhcYl-8G02cJ9rf8zXNaz4zZixyBXnN0w&oe=64C68B43&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 0,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 0,
+        "is_favorite": false
+    },
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841400651489454",
+        "text_post_app_joiner_number": 12108790,
+        "pk": 605077716,
+        "pk_id": "605077716",
+        "username": "halalcorner",
+        "full_name": "Halal Corner #HalaLisMyWay",
+        "is_private": false,
+        "is_verified": true,
+        "profile_pic_id": "2853079800021874054_605077716",
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/285434816_979638742705645_3338512087437922158_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=101&_nc_ohc=f9Z7ZrC9REUAX-zPzGG&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfBlzsqgf7ztWZP9Mvptdw8JcuXj0gb_yVdc3ROOiAotxQ&oe=64C646A5&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 0,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 0,
+        "is_favorite": false
+    },
+    {
+        "has_anonymous_profile_picture": false,
+        "fbid_v2": "17841404706443486",
+        "text_post_app_joiner_number": 8356867,
+        "pk": 4667892881,
+        "pk_id": "4667892881",
+        "username": "dramaojol.id",
+        "full_name": "Drama Ojek Online Indonesia",
+        "is_private": false,
+        "is_verified": false,
+        "profile_pic_id": "3053769319733239405_4667892881",
+        "profile_pic_url": "https://scontent-cgk1-2.cdninstagram.com/v/t51.2885-19/334259663_524214669634471_5642983760181781245_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-cgk1-2.cdninstagram.com&_nc_cat=1&_nc_ohc=wpGnqAtgbVwAX-XSjmy&edm=ALB854YBAAAA&ccb=7-5&oh=00_AfC_0aPjvTXASg_aIOSz-ZUtLEGJo-d0Fz4hgI4Tu2EDQQ&oe=64C7ADFB&_nc_sid=ce9561",
+        "account_badges": [],
+        "is_possible_scammer": false,
+        "third_party_downloads_enabled": 0,
+        "is_possible_bad_actor": {
+            "is_possible_scammer": false,
+            "is_possible_impersonator": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            },
+            "is_possible_impersonator_threads": {
+                "is_unconnected_impersonator": false,
+                "connected_similar_user_id": null
+            }
+        },
+        "latest_reel_media": 0,
+        "is_favorite": false
+    }
+]
